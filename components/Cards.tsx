f5fe93e76216adc29636deddd78a08ed59b251e5
sakkurthi sashank <sakkurthisashank@gmail.com>
2022-06-02 07:45:07 +0530

    Buttons hover

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -28,13 +28,23 @@ const Data = [
     data: "Feb 08, 2021",
   },
   {
-    id: 1,
+    id: 2,
     title: "Cause Of Environment",
     content:
       "Global warming is the long-term heating of Earth's climate system observed since the pre-industrial period (between 1850 and 1900) due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. Pollution is the introduction of contaminants into the natural environment that cause adverse change.[1] Pollution can take the form of any substance (solid, liquid, or gas) or energy (such as radioactivity, heat, sound, or light). Pollutants, the components of pollution, can be either foreign substances/energies or naturally occurring contaminants. Although environmental pollution can be caused by natural events, the word pollution generally implies that the contaminants have an anthropogenic source – that is, a source created by human activities. Pollution is often classed as point source or nonpoint source pollution. In 2015, pollution killed nine million people worldwide.[2][3] Major forms of pollution include air pollution, light pollution, litter, noise pollution, plastic pollution, soil contamination, radioactive contamination, thermal pollution, visual pollution, and water pollution.",
     avatar:
       "https://lh3.googleusercontent.com/a-/AOh14Gi-niaolq7-Wpc97EAD77WzhMpgcyHqeUD1q4Xdig=s360-p-rw-no",
-    author: "Sakkurthi Srinivasa rao",
+    author: "Sakkurthi Srinivasa",
+    data: "Feb 08, 2021",
+  },
+  {
+    id: 3,
+    title: "Cause Of Environment",
+    content:
+      "Global warming is the long-term heating of Earth's climate system observed since the pre-industrial period (between 1850 and 1900) due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. Pollution is the introduction of contaminants into the natural environment that cause adverse change.[1] Pollution can take the form of any substance (solid, liquid, or gas) or energy (such as radioactivity, heat, sound, or light). Pollutants, the components of pollution, can be either foreign substances/energies or naturally occurring contaminants. Although environmental pollution can be caused by natural events, the word pollution generally implies that the contaminants have an anthropogenic source – that is, a source created by human activities. Pollution is often classed as point source or nonpoint source pollution. In 2015, pollution killed nine million people worldwide.[2][3] Major forms of pollution include air pollution, light pollution, litter, noise pollution, plastic pollution, soil contamination, radioactive contamination, thermal pollution, visual pollution, and water pollution.",
+    avatar:
+      "https://lh3.googleusercontent.com/a-/AOh14Gi-niaolq7-Wpc97EAD77WzhMpgcyHqeUD1q4Xdig=s360-p-rw-no",
+    author: "Sakkurthi Srinivasa",
     data: "Feb 08, 2021",
   },
 ];
@@ -70,6 +80,10 @@ export default function blogCard() {
                 _focus={{ boxShadow: "none" }}
                 leftIcon={<AddIcon />}
                 colorScheme="messenger"
+                _hover={{
+                  transform: "translateY(-2px)",
+                  boxShadow: "lg",
+                }}
               >
                 Follow
               </Button>
@@ -123,6 +137,10 @@ export default function blogCard() {
                   _focus={{ boxShadow: "none" }}
                   variant="ghost"
                   color={"#0284c7"}
+                  _hover={{
+                    transform: "translateY(-2px)",
+                    boxShadow: "lg",
+                  }}
                   leftIcon={
                     <BiLike color={"#0ea5e9"} size={"20"} fontWeight={""} />
                   }
@@ -133,6 +151,10 @@ export default function blogCard() {
                   size={"sm"}
                   _focus={{ boxShadow: "none" }}
                   color={"#ea580c"}
+                  _hover={{
+                    transform: "translateY(-2px)",
+                    boxShadow: "lg",
+                  }}
                   variant="ghost"
                   leftIcon={<BiCommentDetail size={"20"} color={"#f97316"} />}
                 >
@@ -142,6 +164,10 @@ export default function blogCard() {
                   size={"sm"}
                   _focus={{ boxShadow: "none" }}
                   variant="ghost"
+                  _hover={{
+                    transform: "translateY(-2px)",
+                    boxShadow: "lg",
+                  }}
                 >
                   <BiShareAlt size={"20"} color={"#475569"} />
                 </Button>
