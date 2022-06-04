ffa1f89024b1afde2cb4ae84410125f6036778a7
sakkurthi sashank <sakkurthisashank@gmail.com>
2022-06-04 07:32:14 +0530

    new page

---
+++ pages/trending.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from "../components/Navbar";
+
+function Trending() {
+    return (
+        <div>
+            <NavBar>
+                <h1>Hello From Trending</h1>
+            </NavBar>
+        </div>
+    );
+}
+
+export default Trending;
